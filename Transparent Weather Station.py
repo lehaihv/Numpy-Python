a20feabba0f82dc24a7f83a2be268bc3f8647c9a
lehaihv <69892340+lehaihv@users.noreply.github.com>
2020-08-26 16:12:15 +0700

    surface pro undated

--- Transparent Weather Station.py
+++ Transparent Weather Station.py
@@ -30,11 +30,14 @@ y = int((hs/2) - (h/2))
 pad_default = 2
 font_size = 20
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+# this removes the maximize button
+root.resizable(0,0)
+#root.attributes('-toolwindow', True)
 #root.wm_attributes('-transparentcolor', root['bg'])
 
 canvas = tk.Canvas(root, height=320, width=480)
 canvas.pack()
-bg_img = tk.PhotoImage(file="wall.gif")#file="C:/Users/bmg/Desktop/eh1.gif")
+bg_img = tk.PhotoImage(file="wall1.gif")#file="C:/Users/bmg/Desktop/eh1.gif")
 bg_label = canvas.create_image((0,0), image=bg_img, anchor=tk.N+tk.W)
 
 now = datetime.now()
@@ -94,7 +97,7 @@ def get_weather():
 	#city = city + 1
 	#lati="21.045021"#"52.194504"
 	#longi="105.800690"#"0.134708"
-	
+	city = 0
 	# Add units=si to get it in sensible ISO units not stupid Fahreneheit.
 	url="https://api.forecast.io/forecast/"+apikey+"/"+lati+","+longi+"?units=si"
 
@@ -113,7 +116,7 @@ def get_weather():
 	canvas.itemconfig(label_hum, text=str(int(hum))+ " %")
 	canvas.itemconfig(label_press, text=str(press)+ " mbar")
 
-	root.after(5000, get_weather)
+	root.after(3600000, get_weather)
 
 
 def show_time():
@@ -127,16 +130,16 @@ def exit():
 	root.quit()
 
 
-label_place = canvas.create_text((240,50), text=cityname, font=("Arial Bold", 45), fill="#652828")
-label_temp = canvas.create_text((240,100), text=temp, font=("Arial Bold", 45), fill="#652828")
-label_hum = canvas.create_text((240,150), text=hum, font=("Arial Bold", 45), fill="#652828")
-label_press = canvas.create_text((240,200), text=press, font=("Arial Bold", 45), fill="#652828")
+label_place = canvas.create_text((240,40), text=cityname, font=("Arial Bold", 45), fill="#894949")
+label_temp = canvas.create_text((240,180), text=temp, font=("Arial Bold", 65), fill="#1b92ed")#652828")
+label_hum = canvas.create_text((100,100), text=hum, font=("Arial Bold", 30), fill="#0D3243")
+label_press = canvas.create_text((350,100), text=press, font=("Arial Bold", 30), fill="#0D3243")
 
-label0_time = canvas.create_text((100,260), text="Time", font=("Arial Bold", 15), fill="#652828")
-label_time = canvas.create_text((100,290), text=current_time, font=("Arial Bold", 25), fill="#652828")
+#label0_time = canvas.create_text((100,260), text="Time", font=("Arial Bold", 15), fill="#652828")
+label_time = canvas.create_text((100,290), text=current_time, font=("Arial Bold", 30), fill="#2489B8")
 
-label0_date = canvas.create_text((350,260), text="Date", font=("Arial Bold", 15), fill="#652828")
-label_date = canvas.create_text((350,290), text=current_date, font=("Arial Bold", 25), fill="#652828")
+#label0_date = canvas.create_text((350,260), text="Date", font=("Arial Bold", 15), fill="#652828")
+label_date = canvas.create_text((350,290), text=current_date, font=("Arial Bold", 30), fill="#2489B8")
 
 root.after(2000, get_weather)
 root.after(1000, show_time)
